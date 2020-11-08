8050b145d68540997309e611990f86ad85d81d4f
Thiago-l <thigo.l17@yahoo.com>
2020-11-08 18:13:24 -0300

    adding the Chakra UI Package

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ChakraProvider } from '@chakra-ui/core';
 import App from './app';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
